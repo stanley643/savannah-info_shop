2a679322ce73d013621f5f80cd6a6df79b1db088
Stanley643 <nmurigi@kabarak.ac.ke>
2024-05-05 10:30:00 +0300

    models

--- hospital/hospital/settings.py
+++ hospital/hospital/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pharmacy',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'hospital.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'shop',
+        'USER': 'postgres',
+        'PASSWORD': '0x150D',
+        'HOST': 'Localhost',
+        'POST': '5432',
     }
 }
 
