c3898ccb618b46476348e5878848b2b249b6bc26
Stanley643 <nmurigi@kabarak.ac.ke>
2024-05-06 22:13:09 +0300

    schema defination

--- hospital/pharmacy/schema.py
+++ hospital/pharmacy/schema.py
@@ -17,7 +17,6 @@ class Query(graphene.ObjectType):
     customers_by_order = graphene.List(CustomerType, order_item=graphene.String())
     total_orders_by_customer = graphene.Int(customer_code=graphene.String())
 
-
     def resolve_all_customers(self, info, **kwargs):
         return Customer.objects.all()
     
@@ -32,5 +31,39 @@ class Query(graphene.ObjectType):
     
     def resolve_total_orders_by_customer(self, info, customer_code, **kwargs):
         return Order.objects.filter(customer__code=customer_code).count()
-    
-schema = graphene.Schema(query=Query)
+
+# Define an input type for the order data
+class OrderInput(graphene.InputObjectType):
+    customer_code = graphene.String(required=True)
+    item = graphene.String(required=True)
+    amount = graphene.Decimal(required=True)
+
+# Mutation for adding a new order
+class AddOrder(graphene.Mutation):
+    class Arguments:
+        order_data = OrderInput(required=True)
+
+    # Define the return fields of the mutation
+    order = graphene.Field(OrderType)
+
+    @staticmethod
+    def mutate(root, info, order_data):
+        # Retrieve the customer object based on the provided code
+        customer = Customer.objects.get(code=order_data.customer_code)
+        
+        # Create a new order object
+        order = Order.objects.create(
+            customer=customer,
+            item=order_data.item,
+            amount=order_data.amount
+        )
+        
+        # Return the created order
+        return AddOrder(order=order)
+
+# Mutation class
+class Mutation(graphene.ObjectType):
+    add_order = AddOrder.Field()
+
+# Schema
+schema = graphene.Schema(query=Query, mutation=Mutation)
