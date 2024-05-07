fb5d8b3749048b4b542b79f26bbfebfd53b2027e
Stanley643 <nmurigi@kabarak.ac.ke>
2024-05-08 00:34:05 +0300

    admin file

--- hospital/pharmacy/admin.py
+++ hospital/pharmacy/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Order, Customer
+from .models import Order, Customer, User, Item
 # Register your models here.
 @admin.register(Customer)
 class Customer(admin.ModelAdmin):
@@ -7,4 +7,12 @@ class Customer(admin.ModelAdmin):
 
 @admin.register(Order)
 class Order(admin.ModelAdmin):
+    pass
+
+@admin.register(User)
+class User(admin.ModelAdmin):
+    pass
+
+@admin.register(Item)
+class Item(admin.ModelAdmin):
     pass
